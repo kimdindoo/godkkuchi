7a219e1304c2102490c6821829177eb493a3ace2
kimdindoo <aaa119o1125@gmail.com>
2025-08-15 23:23:21 +0900

    chore(ios): Configure home widget settings

---
+++ ios/MyHomeWidget/MyHomeWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MyHomeWidgetBundle.swift
+//  MyHomeWidget
+//
+//  Created by 김진수 on 8/15/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MyHomeWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MyHomeWidget()
+    }
+}
