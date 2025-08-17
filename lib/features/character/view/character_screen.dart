a4497a6150dd99e49ee37afc9fbc735741cf4d47
kimdindoo <aaa119o1125@gmail.com>
2025-08-17 23:57:34 +0900

    refactor: Move character screen to a separate file

---
+++ lib/features/character/view/character_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+class CharacterScreen extends StatelessWidget {
+  const CharacterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Character Screen'),
+      ),
+      body: GameWidget(
+        game: MyGame(), // MyGame 인스턴스를 생성하여 게임을 렌더링합니다.
+      ),
+    );
+  }
+}
+
+// FlameGame을 상속받는 MyGame 클래스
+class MyGame extends FlameGame {
+  // 뿌딩이 캐릭터를 나타낼 SpriteComponent 변수
+  late SpriteComponent bbudingCharacter;
+
+  @override
+  Color backgroundColor() => Colors.blue;
+
+  @override
+  Future<void> onLoad() async {
+    // 게임이 시작될 때 한 번만 호출됩니다.
+    super.onLoad();
+
+    // 'images/bbuding.png' 경로의 스프라이트를 비동기로 로드합니다.
+    // 'images/'는 pubspec.yaml에 설정한 경로에 자동으로 추가됩니다.
+    final bbudingSprite = await loadSprite('bbuding.png');
+
+    // SpriteComponent를 생성하여 캐릭터의 속성을 정의합니다.
+    bbudingCharacter = SpriteComponent(
+      sprite: bbudingSprite,
+      size: Vector2(150, 150), // 캐릭터의 크기를 150x150으로 설정
+      anchor: Anchor.center, // 위치 설정의 기준점을 캐릭터의 중심으로 설정
+      position: size / 2, // 게임 화면의 정중앙에 배치
+    );
+
+    // 준비된 캐릭터 컴포넌트를 게임 월드에 추가합니다.
+    add(bbudingCharacter);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,19 @@
-import 'package:flame/components.dart';
-import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:godkkuchi/features/character/view/character_screen.dart';
 
 void main() {
-  // MyGame 클래스의 인스턴스를 생성합니다.
-  final myGame = MyGame();
-
-  // GameWidget을 사용하여 게임을 화면에 렌더링합니다.
   runApp(
-    GameWidget(game: myGame),
+    const MyApp(),
   );
 }
 
-// FlameGame을 상속받는 MyGame 클래스
-class MyGame extends FlameGame {
-  // 뿌딩이 캐릭터를 나타낼 SpriteComponent 변수
-  late SpriteComponent bbudingCharacter;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-  Future<void> onLoad() async {
-    // 게임이 시작될 때 한 번만 호출됩니다.
-    super.onLoad();
-
-    // 'images/bbuding.png' 경로의 스프라이트를 비동기로 로드합니다.
-    // 'images/'는 pubspec.yaml에 설정한 경로에 자동으로 추가됩니다.
-    final bbudingSprite = await loadSprite('bbuding.png');
-
-    // SpriteComponent를 생성하여 캐릭터의 속성을 정의합니다.
-    bbudingCharacter = SpriteComponent(
-      sprite: bbudingSprite,
-      size: Vector2(150, 150), // 캐릭터의 크기를 150x150으로 설정
-      anchor: Anchor.center, // 위치 설정의 기준점을 캐릭터의 중심으로 설정
-      position: size / 2, // 게임 화면의 정중앙에 배치
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: CharacterScreen(),
     );
-
-    // 준비된 캐릭터 컴포넌트를 게임 월드에 추가합니다.
-    add(bbudingCharacter);
   }
 }
